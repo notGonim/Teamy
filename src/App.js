16a1fdae3ec889b634dcd0e05481a3244705825a
notGonim <mhmodgonim96@gmail.com>
2021-02-23 14:51:24 +0200

    Config the routes for the main app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import './App.css';
+import styled from 'styled-components'
 import {
   BrowserRouter as Router, Switch, Route, Link
 } from "react-router-dom";
 import { Header } from './components/header/Header';
+import { Sidebar } from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
 
+      <Route>
+        <>
+          <Header />
+          <Sidebar />
+          <AppBody>
+            <Switch>
+              <Route exact path="/" />
+            </Switch>
+          </AppBody>
+        </>
 
-      <Header></Header>
+      </Route>
+
+
+
     </div>
   );
 }
 
 export default App;
+
+
+const AppBody = styled.div`
+
+`
